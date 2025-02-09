728274a03e803164494b2788c21181f86117f37c
JohamBarberena <alguien_bama@hotmail.com>
2025-02-09 10:05:43 -0600

    Eventos

--- Interfaz/script.js
+++ Interfaz/script.js
@@ -1,18 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Evento para la barra de búsqueda
-    const searchBar = document.querySelector('.search-bar');
-    searchBar.addEventListener('focus', () => {
-        searchBar.style.borderColor = 'blue';
+    const searchBar = document.querySelector('.search');
+    const searchInput = document.querySelector('.search-bar');
+    searchInput.addEventListener('focus', () => {
+        searchBar.style.border = '2px solid var(--detail)';
     });
-    searchBar.addEventListener('blur', () => {
-        searchBar.style.borderColor = '';
+    searchInput.addEventListener('blur', () => {
+        searchBar.style.border = '';
     });
 
     // Evento para las imágenes dentro de las cajas
-    const boxes = document.querySelectorAll('.box img');
+    const boxes = document.querySelectorAll('.gallery_item');
     boxes.forEach(box => {
         box.addEventListener('click', () => {
-            alert(`Has hecho clic en la imagen: ${box.alt}`);
+            alert(`Has hecho clic en el item: ${box.id}`);
+        });
+        
+        box.addEventListener('mouseover', () => {
+            box.style.transform = 'scale(1.05)';
+            box.style.transition = 'transform 0.3s ease-in-out';
+        });
+
+        box.addEventListener('mouseout', () => {
+            box.style.transform = 'scale(1)';
         });
     });
 
